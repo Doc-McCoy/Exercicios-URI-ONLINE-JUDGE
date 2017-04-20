0510c991cf090fff56e4bdbab756c5f2dbf73b93
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 00:49:25 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,11 @@
+# Exercicio referente ao problema numero 1008 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1008
+# Por: Renan Santana Desiderio
+
+numero = input()
+horas = input()
+valor = input()
+salario = horas*valor
+
+print "NUMBER = %i" % numero
+print "SALARY = U$ %.2f" % salario
