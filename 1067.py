bf961d109c2f497003c6265cd790a80740415e57
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 08:12:10 -0300

    Create 1067.py

---
+++ 1067.py
@@ -0,0 +1,10 @@
+# Exercicio referente ao problema numero 1067 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1067
+# Por: Renan Santana Desiderio
+
+c = 0
+i = input()
+while c<=i:
+	if c%2==1:
+		print c
+	c+=1
