c7807ea9b10c25fd038e225fc22e3d8c76d8fd38
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 00:48:16 -0300

    Create 1007.py

---
+++ 1007.py
@@ -0,0 +1,11 @@
+# Exercicio referente ao problema numero 1007 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
+# Por: Renan Santana Desiderio
+
+a=input()
+b=input()
+c=input()
+d=input()
+DIFERENCA = (a*b-c*d)
+
+print "DIFERENCA = %i" %DIFERENCA
