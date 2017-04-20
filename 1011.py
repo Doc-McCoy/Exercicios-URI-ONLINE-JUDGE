6bda1422f3969834bfb7711fe6c041655ef4478d
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 08:07:09 -0300

    Create 1011.py

---
+++ 1011.py
@@ -0,0 +1,9 @@
+# Exercicio referente ao problema numero 1011 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1011
+# Por: Renan Santana Desiderio
+
+raio = input()
+pi = 3.14159
+volume = (4/3.0)*pi*(raio*raio*raio)
+
+print "VOLUME = %.3f" % volume
