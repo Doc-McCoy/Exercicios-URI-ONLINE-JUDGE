c7e9821d2aa9342a5b1e83f960fbd56e5ea94aba
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 00:47:31 -0300

    Update 1006.py

--- 1006.py
+++ 1006.py
@@ -1,5 +1,5 @@
-# Exercicio referente ao problema numero 1002 do URI ONLINE JUDGE.
-# https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+# Exercicio referente ao problema numero 1006 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1006
 # Por: Renan Santana Desiderio
 
 a = input()
