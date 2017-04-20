9055d1e412c9e8f1ad5c4e959eb9667a2ae89c26
Renan Santana Desiderio <renan_ota@hotmail.com>
2017-04-20 00:46:12 -0300

    Create 1006.py

---
+++ 1006.py
@@ -0,0 +1,10 @@
+# Exercicio referente ao problema numero 1002 do URI ONLINE JUDGE.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+# Por: Renan Santana Desiderio
+
+a = input()
+b = input()
+c = input()
+MEDIA = (a*2.0 + b*3.0 + c*5.0)/10.0
+
+print "MEDIA = %.1f" % MEDIA
